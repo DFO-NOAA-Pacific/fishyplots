18f39feb240d381481a77c672440c1660cfc9728
ZoeKhan1 <zkhan@smith.edu>
2025-07-01 11:51:13 -0700

    init length_frequency function

---
+++ R/length_frequency.R
@@ -0,0 +1,24 @@
+#' Function to plot length frequency of fish species
+#'
+#' @param data a data frame made using pull_bio() from a fisheries survey, contains biological info
+#' @return a ggplot object
+#' @importFrom dplyr filter 
+#' @importFrom scales complete
+#' @importFrom ggplot 
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' }
+length_frequency <- function(data) {
+  data_clean <- data |>
+    filter(!is.na(Length_cm), !is.na(Age_years)) |>
+    complete(Year = full_seq(Year, 1))
+  data_clean |>
+    ggplot(aes(x= Length_cm)) +
+    geom_histogram() +
+    facet_wrap(~Year, nrow = 3) +
+    theme_bw() +
+    labs(title = "Length Frequency", x = "Length (cm)", y = "Count")
+}
