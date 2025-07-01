762c8dd85b1482ae0c1df8998a5277d86281d712
ZoeKhan1 <zkhan@smith.edu>
2025-07-01 12:14:15 -0700

    Fixing import issue

--- R/length_frequency.R
+++ R/length_frequency.R
@@ -3,8 +3,9 @@
 #' @param data a data frame made using pull_bio() from a fisheries survey, contains biological info
 #' @return a ggplot object
 #' @importFrom dplyr filter 
-#' @importFrom scales complete
-#' @importFrom ggplot 
+#' @importFrom tidyr complete
+#' @importFrom ggplot2 ggplot aes geom_histogram facet_wrap theme_bw labs
+#' @importFrom scales full_seq 
 #' @export
 #'
 #' @examples
@@ -15,6 +16,7 @@ length_frequency <- function(data) {
   data_clean <- data |>
     filter(!is.na(Length_cm), !is.na(Age_years)) |>
     complete(Year = full_seq(Year, 1))
+  
   data_clean |>
     ggplot(aes(x= Length_cm)) +
     geom_histogram() +
