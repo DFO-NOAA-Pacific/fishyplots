0525256459647266b4ce170053989938db6a846b
Callie Murakami <muracal2@uw.edu>
2025-07-09 11:42:09 -0700

    added unread age structures

--- R/survey_table.R
+++ R/survey_table.R
@@ -1,6 +1,6 @@
 #' Main function to display survey specimen counts
 #'
-#' @param data a data frame of target species including Length_cm, Weight_kg, Age_years, and Year.
+#' @param data a data frame of target species including Length_cm, Weight_kg, Age_years, Otosag_id, and Year.
 #' @param form choose 1 for tibble or 2 for ggplot
 #' @return a tibble or ggplot object
 #' @importFrom dplyr group_by summarize mutate arrange bind_rows
@@ -22,26 +22,40 @@ length_count <- data.frame(length_cm = data$Length_cm, year = data$Year) %>%
   na.omit() %>% #omit rows with no data from being counted
   group_by(year) %>%
   summarize(n_samples=n())%>% #sum sample number over grouping (year)
-  mutate(sample_type = "length") #label as length count
+  mutate(sample_type = "Length") #label as length count
 
 weight_count <- data.frame(weight_cm = data$Weight_kg, year = data$Year) %>%
   na.omit() %>% 
   group_by(year) %>%
   summarize(n_samples=n())%>%
-  mutate(sample_type = "weight")
+  mutate(sample_type = "Weight")
 
 age_count <- data.frame(age_yr = data$Age_years, year = data$Year) %>%
   na.omit() %>% 
   group_by(year) %>%
   summarize(n_samples=n())%>%
-  mutate(sample_type = "age")
+  mutate(sample_type = "Age")
+
+agestr_count <- data.frame(otosag = data$Otosag_id, year = data$Year) %>% 
+  na.omit() %>% 
+  group_by(year) %>%
+  summarize(n_samples=n())%>%
+  mutate(sample_type = "Age Structures") #all structures collected
+
+unread_count <- left_join(agestr_count, age_count, by = "year") %>%
+  mutate(n_ages_read = ifelse(is.na(n_samples.y), 0, n_samples.y),
+    n_samples = n_samples.x - n_ages_read
+  ) %>%
+  select(year, n_samples) %>%
+  mutate(sample_type = "Unread Age Structures") #unread structures only
 
 # put all into one df
 bio_data <-length_count %>%
   bind_rows(weight_count) %>%
   bind_rows(age_count) %>%
+  bind_rows(unread_count) %>% 
   arrange(year) %>%
-  mutate(sample_type=factor(sample_type, levels=c("age", "weight", "length"))) #set order for plotting later
+  mutate(sample_type=factor(sample_type, levels=c("Unread Age Structures", "Age", "Weight", "Length"))) #set order for plotting later
 
 
 # output table format if argument form = 1
