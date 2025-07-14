7b2f0c405213df4c2533ed4aeeeaf5644d5791d5
Callie Murakami <muracal2@uw.edu>
2025-07-14 15:37:21 -0700

    simplified subsetting in fx

--- R/plot_dbi.R
+++ R/plot_dbi.R
@@ -2,7 +2,7 @@
 #'
 #' @param data survey data options loaded in script: nwfsc_biomass, afsc_biomass, pbs_biomass
 #' @param species common name of species of interest. See unique(data$common_name) for options per dataset.
-#' @param subregion subregion required if using data = afsc_biomass or data = pbs_biomass to specify subregion to be plotted. See unique(data$region) for options per dataset.
+#' @param subregion subregion required to specify subregion to be plotted. See unique(data$region) for options per dataset.
 #' @return a ggplot object, plot of design based indicies
 #' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point theme_bw scale_y_continuous xlab ylab
 #' @importFrom here here
@@ -11,10 +11,12 @@
 #'
 #' @examples
 #' \dontrun{
-#' plot_dbi(nwfsc_biomass, "sablefish")
+#' plot_dbi(nwfsc_biomass, "sablefish", "U.S. West Coast")
 #' plot_dbi(afsc_biomass, "sablefish", "U.S. Gulf of Alaska")
 #' plot_dbi(pbs_biomass, "sablefish", "SYN QCS")
 #' }
+
+###### Data #####
 
 #pull data and reformat: 
 load(here::here("data-raw", "nwfsc_biomass.rda")) # nwfsc index data: object nwfsc_biomass
@@ -31,9 +33,13 @@ pbs_biomass <- do.call(rbind, lapply(names(pbs_biomass),function(spec){
           lwr = lowerci,
           upr = upperci) #rename to have same format as NOAA data : object pbs_biomass
 
-# DBI plot func (adapted from shiny version):
+
+##### Function #######
+  
+# DBI plot function
 plot_dbi <- function(dbi_data, species, subregion) {
-    plot <- ggplot2::ggplot(data = if (any(dbi_data$region == "U.S. West Coast")) {subset(dbi_data, dbi_data$common_name == species)}else {subset(dbi_data, dbi_data$common_name == species & dbi_data$region == subregion)}) +
+    
+    plot <- ggplot2::ggplot(data = subset(dbi_data, dbi_data$common_name == species & dbi_data$region == subregion)) +
       ggplot2::geom_ribbon(ggplot2::aes(x = year, ymin = lwr, ymax = upr), fill = "lightgray") +
       ggplot2::geom_line(ggplot2::aes(x = year, y = est)) +
       ggplot2::geom_point(ggplot2::aes(x = year, y = est)) +
@@ -43,6 +49,4 @@ plot_dbi <- function(dbi_data, species, subregion) {
       ggplot2::scale_y_continuous(
         labels = function(x) format(x, big.mark = ",", scientific = FALSE))
     return(plot)
-}
-
-## standardized index TBD
+  }
