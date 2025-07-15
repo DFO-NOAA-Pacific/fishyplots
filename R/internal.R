bc57f06c53c9a52f1b6433a9fc3635b297d05e12
Callie Murakami <muracal2@uw.edu>
2025-07-15 16:38:46 -0700

    internals script setup and example

--- R/internal.R
+++ R/internal.R
@@ -1,12 +1,8 @@
-# make an internal
-species_list <- c(1,2,3)
-x <- c( 4,5,6)
+#Internals
 
-
-usethis::use_data(species_list = species_list, x = x, internal = TRU
-          
-data <- data.frame(false = 1:10, fal = letters[1:10])        
-data$false
-data$fal
-data$fa
-data[["fal"]]
+### Example: 
+# species_list <- c(1,2,3)
+# x <- c( 4,5,6)
+# 
+# # all objects added to same usethis save
+# usethis::use_data(species_list = species_list, x = x, internal = TRUE)
