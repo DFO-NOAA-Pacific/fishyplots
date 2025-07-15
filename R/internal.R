815c1f61cd25d66965e565b92da66c7e441b28b3
Callie Murakami <muracal2@uw.edu>
2025-07-15 16:38:17 -0700

    script to make all.dbi dataset

---
+++ R/internal.R
@@ -0,0 +1,12 @@
+# make an internal
+species_list <- c(1,2,3)
+x <- c( 4,5,6)
+
+
+usethis::use_data(species_list = species_list, x = x, internal = TRU
+          
+data <- data.frame(false = 1:10, fal = letters[1:10])        
+data$false
+data$fal
+data$fa
+data[["fal"]]
