8390f6c9a1f6b821fdd0253d2ebfd2398174f35a
Callie Murakami <muracal2@uw.edu>
2025-07-15 16:32:57 -0700

    edit description

--- R/plot_dbi.R
+++ R/plot_dbi.R
@@ -1,7 +1,7 @@
 #' Function and formatted data to plot design based index
 #'
-#' @param species common name of species of interest. See unique(all.dbi$common_name) for options per dataset.
-#' @param reg region, required to specify region to be plotted. See unique(data$region) for options per dataset.
+#' @param species common name of species of interest. See examples for options per dataset.
+#' @param reg region, required to specify region to be plotted. See examples for options per dataset.
 #' @return a ggplot object, plot of design based indicies
 #' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point theme_bw scale_y_continuous xlab ylab
 #' @importFrom dplyr %>% select rename
@@ -21,17 +21,18 @@
 #' distinct(region)
 #' 
 #' 
-#' # usage example
-#' 
+#' # usage examples
 #' plot_dbi("sablefish", "U.S. West Coast")
 #' plot_dbi("Dover sole", "U.S. Gulf of Alaska")
-#' plot_dbi("arrowtooth flounder", "SYN QCS")
+#' plot_dbi("arrowtooth flounder", "SYN QCS", big_mark = " ")
 #' }
 
 
   
 # DBI plot function
 plot_dbi <- function(species, reg) {
+  
+  # if (!species %in% species_list) stop("") expand later with error messages
   
   data(all.dbi)
     
@@ -47,3 +48,4 @@ plot_dbi <- function(species, reg) {
     return(plot)
   }
    
+
