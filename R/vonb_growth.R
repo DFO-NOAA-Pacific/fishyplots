a8fe68f4c5c833989e8940673c1c3d0aa811070c
ZoeKhan1 <zkhan@smith.edu>
2025-07-17 15:20:34 -0700

    Updated growth function to take in Canada and Alaska data

--- R/vonb_growth.R
+++ R/vonb_growth.R
@@ -10,27 +10,38 @@
 #'
 #' @examples
 #' \dontrun{
-#' vonb_growth(bio_data)
+#' US West Coast
+#' load("data/nwfsc_bio.rda")
+#' data <- nwfsc_bio |> filter(common_name == "arrowtooth flounder")
+#' vonb_growth(data)
+#' 
+#' Canada
+#' load("data/pbs_bio.rda")
+#' data <- pbs_bio |> filter(common_name == "arrowtooth flounder")
+#' vonb_growth(data)
+#' 
+#' Alaska
+#' load("data/afsc_bio.rda")
+#' data <- afsc_bio |> filter(common_name == "arrowtooth flounder")
+#' vonb_growth(data)
 #' }
-
-
 vonb_growth <- function(data) {
   # Clean data
   data_clean <- data |>
-    filter(!is.na(Length_cm)) |>
-    filter(!is.na(Age_years)) |>
-    filter(Sex == "F" | Sex == "M")
+    filter(!is.na(length_cm)) |>
+    filter(!is.na(age_years)) |>
+    filter(sex == "F" | sex == "M")
   
   # Define the von Bertalanffy growth function
-  vb <- Length_cm ~ Linf * (1 - exp(-K * (Age_years - t0)))
+  vb <- length_cm ~ Linf * (1 - exp(-K * (age_years - t0)))
   
-  # Subset data by Sex
-  data_male <- subset(data_clean, Sex == "M")
-  data_female <- subset(data_clean, Sex == "F")
+  # Subset data by sex
+  data_male <- subset(data_clean, sex == "M")
+  data_female <- subset(data_clean, sex == "F")
   
   # Get starting values for Linf, K, and t0 using FSA package
-  starts_male <- findGrowthStarts(formula = Length_cm ~ Age_years, data = data_male)
-  starts_female <- findGrowthStarts(formula = Length_cm ~ Age_years, data = data_female)
+  starts_male <- findGrowthStarts(formula = length_cm ~ age_years, data = data_male)
+  starts_female <- findGrowthStarts(formula = length_cm ~ age_years, data = data_female)
   
   # Fit models using the vb and starts
   model_male <- nls(vb, data = data_male, start = starts_male)
@@ -40,28 +51,28 @@ vonb_growth <- function(data) {
   xx <- coef(model_female) |> round(2)
   
   # Extract all unique ages
-  age_seq_male <- sort(unique(data_male$Age_years))
-  age_seq_female <- sort(unique(data_female$Age_years))
+  age_seq_male <- sort(unique(data_male$age_years))
+  age_seq_female <- sort(unique(data_female$age_years))
   
   # Create data frames for predictions
   pred_male <- data.frame(
-    Age_years = age_seq_male,
-    fit = predict(model_male, newdata = data.frame(Age_years = age_seq_male)),
-    Sex = "M")
+    age_years = age_seq_male,
+    fit = predict(model_male, newdata = data.frame(age_years = age_seq_male)),
+    sex = "M")
   
   pred_female <- data.frame(
-    Age_years = age_seq_female,
-    fit = predict(model_female, newdata = data.frame(Age_years = age_seq_female)),
-    Sex = "F")
+    age_years = age_seq_female,
+    fit = predict(model_female, newdata = data.frame(age_years = age_seq_female)),
+    sex = "F")
   
   # Combine predictions
   growth_preds <- rbind(pred_male, pred_female)
   
   # Plot growth function
-  ggplot(data = data_clean, aes(x = Age_years, y = Length_cm, color = Sex)) +
+  ggplot(data = data_clean, aes(x = age_years, y = length_cm, color = sex)) +
     geom_jitter(alpha = 0.1) +
     scale_color_manual(values = c("M" = "#008b8b", "F" = "#daa520")) +
-    geom_line(data = growth_preds, aes(x = Age_years, y = fit, color = Sex), inherit.aes = FALSE) +
+    geom_line(data = growth_preds, aes(x = age_years, y = fit, color = sex), inherit.aes = FALSE) +
     theme_bw() +
     labs(x = "Age (years)", y = "Length (cm)", title = "Growth") +
     annotate("label", x = Inf, y = -Inf, label = paste0("k = ", xx[2], "; Lmin = ", xx[3], "; Linf = ", xx[1]), 
