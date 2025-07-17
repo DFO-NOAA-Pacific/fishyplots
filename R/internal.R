bf03c3debe5bf8fc87d579e962c0e4f63ed4c86f
Callie Murakami <muracal2@uw.edu>
2025-07-16 17:33:33 -0700

    internal for converting sex codes

--- R/internal.R
+++ R/internal.R
@@ -6,3 +6,26 @@
 # 
 # # all objects added to same usethis save
 # usethis::use_data(species_list = species_list, x = x, internal = TRUE)
+
+
+#' Convert numeric sex codes for standardization
+#'
+#' Converts 1 -> M, 2 -> F, 0 or 3 -> U, any other to NA
+#' Internal function.
+#'
+#' @param x vector of sex codes
+#' @return standardized sex characters
+#' @noRd
+
+# function to convert numeric sex codes to M, F and U (Unknown)
+convert_sex <- function(x) { 
+  x <- as.character(x)
+  x <- dplyr::case_when(
+    x == 1 ~ "M",
+    x == 2 ~ "F",
+    x %in% c("0", "3") ~ "U",
+    TRUE ~ NA_character_  # set unknown or unexpected values as NA
+  )
+}
+
+
