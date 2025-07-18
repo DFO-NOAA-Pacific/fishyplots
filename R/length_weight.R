d630dc5fa7cbef06eb4b882d3f4b05fde40d6098
Callie Murakami <muracal2@uw.edu>
2025-07-18 13:27:47 -0700

    plot using lw_vb_predictions fits

--- R/length_weight.R
+++ R/length_weight.R
@@ -1,6 +1,6 @@
 #' Main function to plot length-weight relationships by sex
 #'
-#' @param data a data frame of biological data
+#' @param data science center bio data to be plotted. Preload from data file.
 #' @param species common or scientific name of target species
 #' @param color default FALSE for greyscale, TRUE gives red and blue
 #' @return a plot of sexed data with log regression slope and intercept
@@ -17,37 +17,24 @@
 
 length_weight <- function(data, species, color = FALSE) #color BW default
 { 
-  # transform and regression
-  #remove unsexed and NAs
-  log_data <- data %>% 
-    filter(species == common_name | species == scientific_name) %>% 
-    filter(!sex == "U") |> 
-    filter(!is.na(length_cm)) %>%  
-    filter(!is.na(weight_kg)) %>%  
-    mutate(loglength = log(length_cm), logweight = log(weight_kg))
-  
-  #subset male and female for ease
-  male <- subset(log_data, log_data$sex == "M")
-  female <- subset(log_data, log_data$sex == "F")
-  
-  #regression
-  lw_mod_M <- lm(logweight ~ loglength, data = male)
-  lw_mod_F <- lm(logweight ~ loglength, data = female)
-  
-  #create regression predictions
-  log_predict_M <- data.frame(predict(lw_mod_M, interval = "confidence"))
-  log_predict_F <- data.frame(predict(lw_mod_F, interval = "confidence"))
-  
-  real_predict_M <- apply(X = log_predict_M, MARGIN = 2, FUN = exp)
-  real_predict_F <- apply(X = log_predict_F, MARGIN = 2, FUN = exp)
-  
-  # combine for legend plotting
-  real_predict_all <- rbind(
-    data.frame(length_cm = male$length_cm, fit = real_predict_M, sex = "M"),
-    data.frame(length_cm = female$length_cm, fit = real_predict_F, sex = "F")
-  )
-  
-  
+  # # transform and regression
+  # #remove unsexed and NAs
+  # log_data <- data %>% 
+  #   filter(species == common_name | species == scientific_name) %>% 
+  #   filter(!sex == "U") |> 
+  #   filter(!is.na(length_cm)) %>%  
+  #   filter(!is.na(weight_kg)) %>%  
+  #   mutate(loglength = log(length_cm), logweight = log(weight_kg))
+  # 
+  # #subset male and female for ease
+  # male <- subset(log_data, log_data$sex == "M")
+  # female <- subset(log_data, log_data$sex == "F")
+  # 
+  # #regression
+  # lw_mod_M <- lm(logweight ~ loglength, data = male)
+  # lw_mod_F <- lm(logweight ~ loglength, data = female)
+  # 
+
   
   #plot with or without color
   if(color == TRUE){
@@ -57,13 +44,45 @@ length_weight <- function(data, species, color = FALSE) #color BW default
   sex.color <- c("M" = "grey30", "F" = "black")
   }
   
-  plot <- ggplot(log_data, aes(x = length_cm, y = weight_kg)) +
+  #load fit dataset
+  data("lw_vb_predictions")
+  
+  #subset data to species
+  spec.data <- data  %>% 
+    filter(species == common_name | species == scientific_name) %>%
+    filter(!sex == "U") |>
+    filter(!is.na(length_cm)) %>%
+    filter(!is.na(weight_kg))
+  
+  # get regression fit for m and f of species in data
+  F_pred <- lw_vb_predictions %>% 
+    filter(center == unique(spec.data$science_center), species == common | species == scientific, sex == "F")
+  M_pred <- lw_vb_predictions %>% 
+    filter(center == unique(spec.data$science_center), species == common | species == scientific, sex == "M")
+  
+  
+  pred_df_F <- data.frame(
+    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100)
+  ) %>%
+    mutate(y = F_pred$a * x^F_pred$b)
+  pred_df_M <- data.frame(
+    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100)
+  ) %>%
+    mutate(y = M_pred$a * x^M_pred$b)
+  
+  # combine for legend plotting
+  predict_all <- rbind(
+    data.frame(fit = pred_df_M, sex = "M"),
+    data.frame(fit = pred_df_F, sex = "F")
+  )
+  
+  
+  plot <- ggplot(spec.data, aes(x = length_cm, y = weight_kg)) +
     theme_classic() + #removes grid
     geom_jitter(aes(color = sex, shape = sex, fill = sex), alpha = 0.1) +
     #DATA
-    
-    geom_line(data = real_predict_all, 
-              aes(x = length_cm, y = fit.fit, linetype = sex, color = sex), linewidth = 1) + # plot fit lines
+    geom_line(data = predict_all, 
+              aes(x = fit.x, y = fit.y, linetype = sex, color = sex), linewidth = 1) + # plot fit lines
     
     #set M vs F color, shape, lines, fill
     scale_linetype_manual(
@@ -90,13 +109,13 @@ length_weight <- function(data, species, color = FALSE) #color BW default
     # Add text annotations for slope/intercept
     annotate("text", x = -Inf, y = Inf, 
              label = paste0("Female", 
-                            "\n a = ", format(exp(coef(lw_mod_F)[1]), digits = 3, scientific = TRUE),
-                            "\n   b = ", round(coef(lw_mod_F)[2],2)), 
+                            "\n a = ", format(F_pred$a, digits = 3, scientific = TRUE),
+                            "\n   b = ", round(F_pred$b,2)), 
              hjust = -0.5, vjust = 2, size = 4) +
     annotate("text", x = -Inf, y = Inf, 
              label = paste0("  Male", 
-                            "\n a = ", format(exp(coef(lw_mod_M)[1]), digits = 3, scientific = TRUE),
-                            "\n   b = ", round(coef(lw_mod_M)[2],2)), 
+                            "\n a = ", format(M_pred$a, digits = 3, scientific = TRUE),
+                            "\n   b = ", round(M_pred$b,2)), 
              hjust = -0.5, vjust = 3.5, size = 4)
   
   
