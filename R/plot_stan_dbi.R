3d25d67880b6bbc6e4540b9f0f82bc4d540f04bb
Callie Murakami <muracal2@uw.edu>
2025-07-22 11:25:38 -0700

    add standardized CIs

--- R/plot_stan_dbi.R
+++ R/plot_stan_dbi.R
@@ -1,7 +1,7 @@
 #' Function and formatted data to plot standardized design based index
 #'
-#' @param species common name of species of interest. See unique(data$common_name) for options per dataset.
-#' @param regions center or subregions to be plotted. Takes a character list of region names OR a single center name ( "NWFSC", "AFSC", or "PBS") . See unique(data$region) for subregion options per center dataset.
+#' @param species common or scientific name of species of interest. 
+#' @param regions center or subregions to be plotted. Takes a character list of region names OR a single center name ( "NWFSC", "AFSC", or "PBS").
 #' @return a ggplot object, plot of standardized design based indicies of 1+ surveys. 
 #' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point theme_bw scale_y_continuous xlab ylab
 #' @importFrom dplyr %>% select rename
@@ -16,13 +16,13 @@
 plot_stan_dbi <- function(species, regions) {
   
  data(all.dbi)
+ 
   
   # Assign subregions to larger survey center if argument is a single center name
   if (is.character(regions) && length(regions) == 1) {
     if (regions == "AFSC") {
       regions <- c("U.S. Gulf of Alaska", "U.S. Aleutian Islands", "U.S. Eastern Bering Sea Slope", 
-                   "U.S. Eastern Bering Sea Standard Plus NW Region", 
-                   "U.S. Eastern Bering Sea Standard Region", "U.S. Northern Bering Sea")
+                   "U.S. Eastern Bering Sea Standard Plus NW Region", "U.S. Northern Bering Sea")
     } else if (regions == "PBS") {
       regions <- c("SYN QCS","SYN WCVI", "SYN HS", "SYN WCHG")
     } else if (regions == "NWFSC") {
@@ -42,11 +42,14 @@ plot_stan_dbi <- function(species, regions) {
     stand_data <- data %>%
       dplyr::group_by(common_name, region) %>%
       dplyr::filter(!is.na(est)) %>%
-      dplyr::mutate(stand_est = est / mean(est))
+      dplyr::mutate(stand_est = est / mean(est), 
+        stand_lwr = lwr / mean(est),
+        stand_upr = upr / mean(est))
+      
     
     # Filter for selected species and selected regions
     filtered <- stand_data %>%
-      dplyr::filter(common_name == species, region %in% regions)
+      dplyr::filter(common_name == species|scientific_name == species, region %in% regions)
     
     # Add to combined dataframe
     combined_df <- dplyr::bind_rows(combined_df, filtered)
@@ -57,6 +60,7 @@ plot_stan_dbi <- function(species, regions) {
                           ggplot2::aes(x = year, y = stand_est, color = region)) +
     ggplot2::geom_line(linewidth = 1) +
     ggplot2::geom_point() +
+    ggplot2::geom_ribbon(ggplot2::aes(x = year, ymin = stand_lwr, ymax = stand_upr, fill = region), color = NA, alpha = 0.2) +
     ggplot2::ylab("Standardized Biomass Index") +
     ggplot2::xlab("Year") +
     ggplot2::ggtitle(species)  +
