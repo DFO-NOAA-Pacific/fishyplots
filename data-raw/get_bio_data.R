f201044e8ac273f497aab88dd6914cbe2b056607
Callie Murakami <muracal2@uw.edu>
2025-07-23 11:00:07 -0700

    change col name to region, add survey

--- data-raw/get_bio_data.R
+++ data-raw/get_bio_data.R
@@ -10,8 +10,8 @@ nwfsc_survey <- nwfscSurvey::pull_bio(survey = "NWFSC.Combo", common_name = NWFS
 
 # tidy names, select columns of interest
 nwfsc_bio <- janitor::clean_names(nwfsc_survey) %>%
-  dplyr::mutate(science_center = "NWFSC") %>% # add science center
-  dplyr::select(science_center, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years, otosag_id) #otosag_id needed for age structures calculation in survey_table function
+  dplyr::mutate(region = "NWFSC", survey = "NWFSC") %>% # add science center
+  dplyr::select(region, survey, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years, otosag_id) #otosag_id needed for age structures calculation in survey_table function
 
 ###### AFSC data ######
 
@@ -19,14 +19,14 @@ afsc_haul  <- readRDS("data-raw/afsc-haul.rds")
 afsc_specimen  <- readRDS("data-raw/afsc-specimen.rds")
 afsc_spp_list <- read.csv("data-raw/afsc_joined.csv")
 
-afsc_years <- data.frame(event_id = afsc_haul$event_id, year = afsc_haul$year) #haul IDs and years conducted
+afsc_years <- data.frame(event_id = afsc_haul$event_id, year = afsc_haul$year, survey = afsc_haul$survey_name) #haul IDs and years conducted, and survey
 
 afsc_bio <- dplyr::inner_join(afsc_specimen, afsc_years, by = "event_id") %>% # join years by haul ID
   dplyr::filter(common_name %in% afsc_spp_list$common_name) %>% # keep only species of interest
-  dplyr::mutate(science_center = "AFSC") %>%  # add science center name
+  dplyr::mutate(region = "AFSC") %>%  # add science center name
   dplyr::mutate("length_cm" = length_mm/10, "weight_kg" = weight_g/1000, sex = convert_sex(sex)) %>% #convert mm -> cm and g -> kg, convert numeric sex
   dplyr::rename(age_years = age) %>% 
-  dplyr::select(science_center, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years)
+  dplyr::select(region, survey, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years)
   
 ###### PBS data ######
 
@@ -36,13 +36,13 @@ pbs_spp_list <- read.csv("data-raw/pbs_joined.csv")
 pbs_bio <- do.call(rbind, lapply(pbs_spp_list$common_name,function(spec){
     df <- pbs_data[[spec]]$survey_samples #pull $survey_samples df from each species
     return(df)}))%>% #apply function to each species data in list, bind together
-  dplyr::mutate(science_center = "PBS") %>%
+  dplyr::mutate(region = "PBS") %>%
   dplyr::filter(usability_code == c(0, 1, 2, 6)) %>% #filter for usability codes
   dplyr::mutate("weight_kg" = weight/1000, sex = convert_sex(sex)) %>% #g -> kg, convert sex
-  dplyr::rename(length_cm = length, common_name = species_common_name, scientific_name = species_science_name, age_years = age) %>% 
-  dplyr::select(science_center, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years)
+  dplyr::rename(length_cm = length, common_name = species_common_name, scientific_name = species_science_name, age_years = age, survey = survey_abbrev) %>% 
+  dplyr::select(region, survey, year, common_name, scientific_name, sex, length_cm, weight_kg, age_years)
 
 #alternative: using tidy_lengths_raw() and tidy_ages_raw() from gfplot package
 
 #save bio datasets 
-usethis::use_data(nwfsc_bio, pbs_bio, afsc_bio)
+usethis::use_data(nwfsc_bio, pbs_bio, afsc_bio, overwrite = TRUE)
