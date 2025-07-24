66913037b8cca4d3bf1f9d93a75221be2e0f1112
Callie Murakami <muracal2@uw.edu>
2025-07-24 15:24:51 -0700

    new color and survey grouping

--- R/survey_table.R
+++ R/survey_table.R
@@ -13,13 +13,15 @@
 #'
 #' @examples
 #' \dontrun{
-#' data(pbs_bio)
-#' data(afsc_bio)
-#' data(nwfsc_bio)
-#' data <- bind_rows(pbs_bio, afsc_bio, nwfsc_bio)
+#' data("nwfsc_bio")
+#' data("pbs_bio")
+#' data("afsc_bio")
+#' akgulf_bio <- afsc_bio %>% filter(survey == "AK GULF")
+#' akbsai_bio <- afsc_bio %>% filter(survey == "AK BSAI")
+#' all.data <- bind_rows(akgulf_bio, akbsai_bio, nwfsc_bio, pbs_bio)
 #' 
 #' 
-#' survey_table(afsc_bio, "atka mackerel", form = 1)
+#' survey_table(akgulf_bio, "atka mackerel", form = 1)
 #' survey_table(nwfsc_bio, "sebastes zacentrus", form = 2)
 #' survey_table(pbs_bio,"arrowtooth flounder", form = 2)
 #' }
@@ -36,58 +38,76 @@ survey_table <- function(data, species, form = c(1,2)) {
   
   #make subsets with just the wanted data (l/w/a and year)
   #number of lengths counted per year
-  if(any(spec.data$region == "AFSC")){
+  length_count <- data.frame(length = spec.data$length_cm, yr = spec.data$year, survey = spec.data$survey) %>%
+    na.omit() %>% #omit rows with no data from being counted
+    group_by(yr, survey) %>%
+    summarize(n_samples=n())%>% #sum sample number over grouping (year)
+    mutate(sample_type = "Length") #label as length count
+  
+  if(any(spec.data$region == "AFSC")){ #special dataset for AFSC lengths, overwrite previous lengths for AFSC surveys
     data("ak_survey_lengths")
-    length_count <-  ak_survey_lengths %>% 
-      group_by(year) %>%
+    ak_length_count <-  ak_survey_lengths %>% 
+      group_by(year, survey) %>%
       summarize(n_samples=sum(length.count))%>%
       mutate(sample_type = "Length") %>% 
       rename(yr = year) %>% 
-      na.omit}
-  else{
-length_count <- data.frame(length = spec.data$length_cm, yr = spec.data$year) %>%
-  na.omit() %>% #omit rows with no data from being counted
-  group_by(yr) %>%
-  summarize(n_samples=n())%>% #sum sample number over grouping (year)
-  mutate(sample_type = "Length") #label as length count
-}
+      na.omit()
     
+    #which afsc surveys are being called
+    which.asfc<- unique(spec.data$survey[spec.data$region == "AFSC"])
+    
+    # Only replace those surveys
+    length_count <- length_count %>%
+      filter(!(survey %in% which.asfc)) %>%
+      bind_rows(ak_length_count %>% filter(survey %in% which.asfc))
+  }
 
+    
 #weight counts
-weight_count <- data.frame(weight = spec.data$weight_kg, yr = spec.data$year) %>%
+weight_count <- data.frame(weight = spec.data$weight_kg, yr = spec.data$year, survey = spec.data$survey) %>%
   na.omit() %>% 
-  group_by(yr) %>%
+  group_by(yr, survey) %>%
   summarize(n_samples=n())%>%
   mutate(sample_type = "Weight")
 
 #age counts, fill with 0 if no ages taken (ex nwfsc Sebastes zacentrus)
-age_count <- data.frame(age = spec.data$age_years, yr = spec.data$year) %>%
+age_count <- data.frame(age = spec.data$age_years, yr = spec.data$year, survey = spec.data$survey) %>%
   na.omit() %>% 
-  group_by(yr) %>%
+  group_by(yr, survey) %>%
   summarize(n_samples=n())%>%
   mutate(sample_type = "Age")
 if(nrow(age_count) == 0) {
   age_count <- data.frame(n_samples = 0, yr = weight_count$yr, sample_type = "Age")}
 
 # unread age structures: nwfsc has otolith column for this, otherwise use age=NA as unread
-if ("otosag_id" %in% names(data)) {
-agestr_count <- data.frame(otosag = spec.data$otosag_id, yr = spec.data$year) %>% 
-  na.omit() %>% 
-  group_by(yr) %>%
+agestr_count <-  data.frame(age = spec.data$age_years, yr = spec.data$year, survey = spec.data$survey) %>% 
+  group_by(yr, survey) %>%
   summarize(n_samples=n())%>%
-  mutate(sample_type = "Age Structures") #all structures collected
+  mutate(sample_type = "Age Structures") 
+
+if ("otosag_id" %in% names(data)) { #overwrite if dealing with nwfsc data
+nwfsc_count <- spec.data %>%
+  filter(region == "NWFSC", !is.na(otosag_id)) %>%
+  group_by(survey, yr = year) %>%
+  summarize(n_samples = n(), .groups = "drop") %>%
+  mutate(sample_type = "Age Structures (otosag_id)")
+
+# Count for other regions using age_years
+other_count <- data %>%
+  filter(region != "NWFSC", !is.na(age_years)) %>%
+  group_by(survey, yr = year) %>%
+  summarize(n_samples = n(), .groups = "drop") %>%
+  mutate(sample_type = "Age Structures (age_years)")
+
+# Combine both into agestr_count
+agestr_count <- bind_rows(nwfsc_count, other_count)
 }
-else {
-  agestr_count <-  data.frame(age = spec.data$age_years, yr = spec.data$year) %>% 
-    group_by(yr) %>%
-    summarize(n_samples=n())%>%
-    mutate(sample_type = "Age Structures") 
-}
-unread_count <- left_join(agestr_count, age_count, by = "yr") %>%
+
+unread_count <- left_join(agestr_count, age_count, by = c("yr","survey")) %>%
   mutate(n_ages = ifelse(is.na(n_samples.y), 0, n_samples.y), #change NA to 0 for calculation ease
          n_unread = n_samples.x - n_ages, #unread age strugtures = n age str - n ages read
          n_samples = ifelse(n_unread == 0, NA, n_unread)) %>% # make 0 into NA, use common name
-  select(yr, n_samples) %>%
+  select(survey, yr, n_samples) %>%
   mutate(sample_type = "Unread Age Structures") %>% #unread structures only
   na.omit() #omit NAs where n age structures = n read ages
 
@@ -129,7 +149,11 @@ bio_data$label <- ifelse(bio_data$n_samples < 100,
 plot <- ggplot(bio_data, aes(yr, sample_type)) +
   ggplot2::geom_tile(aes(fill = n_samples, alpha = n_samples != 0), colour = "white", show.legend = FALSE) + # to preserve rows where all years have 0 data, color 0 tiles white
   scale_alpha_manual(values = c("TRUE" = 1, "FALSE" = 0)) +
-  scale_fill_gradient(low = "white", high = "#11aedf") + 
+  scale_fill_gradientn(
+    colours = c("#ffffb2", "#fecc5c", "#fd8d3c", "#e31a1c", "red4"),
+    trans = "log10",
+    na.value = "white"
+  ) + 
   theme_pbs() +
   theme(
     axis.ticks.x = element_blank(),
@@ -143,8 +167,8 @@ plot <- ggplot(bio_data, aes(yr, sample_type)) +
   ggplot2::scale_y_discrete(position = "left")+
   scale_x_continuous(breaks = seq(min(bio_data$yr)+1, max(bio_data$yr), by = 2))+ # keep year labels on even years
   ggplot2::ggtitle(paste("Survey Specimen Counts -", unique(spec.data$common_name))) +
-  coord_cartesian(expand = FALSE)
-
+  coord_cartesian(expand = FALSE) +
+  facet_wrap(~ survey, ncol = 1)
 return(plot)
 }
 }
