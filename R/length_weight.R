e329fe745d3c0b888401829b7ebb06af6826de45
Callie Murakami <muracal2@uw.edu>
2025-07-24 13:26:28 -0700

    fix for shiny, remove grey option

--- R/length_weight.R
+++ R/length_weight.R
@@ -2,7 +2,6 @@
 #'
 #' @param data region bio data to be plotted. Preload from data file.
 #' @param species common or scientific name of target species
-#' @param color default TRUE for color, FALSE  for greyscale
 #' @param subset default TRUE for a faster plotting subset of n = 2000. Set FALSE for all available data.
 #' @return a plot of sexed data with log regression slope and intercept
 #' @importFrom ggplot2 ggplot aes geom_point geom_line scale_linetype_manual theme_classic theme element_blank element_text xlab ylab annotate
@@ -19,18 +18,12 @@
 #' akbsai_bio <- afsc_bio %>% filter(survey == "AK BSAI")
 #' all.data <- bind_rows(akgulf_bio, akbsai_bio, nwfsc_bio, pbs_bio)
 #' 
-#' length_weight(akgulf_bio, "atka mackerel", color = T)
+#' length_weight(akgulf_bio, "atka mackerel")
 #' }
 
-length_weight <- function(data, species, color = TRUE, subset = TRUE) { #color default, subset default
-  
-  #plot with or without color
-  if(color == TRUE){
+length_weight <- function(data, species, subset = TRUE) { # subset default
+
   sex.color <- c("M" = "#E69F00", "F" = "#009E73")
-  }
-  else{
-  sex.color <- c("M" = "grey30", "F" = "black")
-  }
   
   #load fit dataset
   data("lw_predictions")
@@ -44,39 +37,39 @@ length_weight <- function(data, species, color = TRUE, subset = TRUE) { #color d
       filter(!is.na(weight_kg)) }
   else (stop(paste("Species name", "'", species,"'", "not found in this dataset.")))
   
-  spec.data <- data  %>% 
-    filter(species == common_name | species == scientific_name) %>%
-    filter(!sex == "U") |>
-    filter(!is.na(length_cm)) %>%
-    filter(!is.na(weight_kg))
-  
   if(subset == TRUE){
     spec.data <- spec.data %>% slice_sample(n = 2000)
     if(nrow(spec.data) < 2000) {message ("Note: Species data less than 2000 rows; plotting all data with no subset")}
     else {message (paste0("Note: subset = TRUE ; plotting a random n = 2000 subset of ", unique(spec.data$common_name), ". Model values not impacted."))}
   }
-  
+  predict_all <- data.frame()
+  for(survey_name in unique(spec.data$survey)) {
   # get regression fit for m and f of species in data
   F_pred <- lw_predictions %>% 
-    filter(survey == unique(spec.data$survey), species == common | species == scientific, sex == "F")
+    filter(survey == survey_name, species == common | species == scientific, sex == "F")
   M_pred <- lw_predictions %>% 
-    filter(survey == unique(spec.data$survey), species == common | species == scientific, sex == "M")
+    filter(survey == survey_name, species == common | species == scientific, sex == "M")
   
   
   pred_df_F <- data.frame(
-    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100)
+    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100),
+    survey = survey_name
   ) %>%
     mutate(y = F_pred$a * x^F_pred$b)
   pred_df_M <- data.frame(
-    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100)
+    x = seq(min(spec.data$length_cm), max(spec.data$length_cm), length.out = 100),
+    survey = survey_name
   ) %>%
     mutate(y = M_pred$a * x^M_pred$b)
   
   # combine for legend plotting
-  predict_all <- rbind(
+  predict_all <- rbind(predict_all,
     data.frame(fit = pred_df_M, sex = "M"),
     data.frame(fit = pred_df_F, sex = "F")
-  )
+  ) 
+  }
+predict_all <- predict_all%>% 
+  rename(survey = fit.survey)
   
   
   plot <- ggplot(spec.data, aes(x = length_cm, y = weight_kg)) +
@@ -100,27 +93,27 @@ length_weight <- function(data, species, color = TRUE, subset = TRUE) { #color d
       values = sex.color,
       labels = c("M" = "Male", "F" = "Female")) +
     
+    facet_wrap( ~ survey) +
     #LEGEND AND LABELS
     #add legend for lines
     theme(legend.title = element_blank(), legend.position.inside  = c(0.9, 0.1), legend.text=element_text(size=10), legend.key.width = unit(1, 'cm')) + # legend position
     xlab("Length (cm)") +# for the x axis label
     ylab("Weight (kg)")+
     ggtitle(unique(spec.data$common_name)) +
-    guides(color = guide_legend(override.aes = list(alpha = 0.5)))+ # increase alpha of legend
+    guides(color = guide_legend(override.aes = list(alpha = 0.5))) # increase alpha of legend
     
-    # ANNOTATIONS
-    # Add text annotations for slope/intercept
-    annotate("text", x = -Inf, y = Inf, 
-             label = paste0("Female", 
-                            "\n a = ", format(F_pred$a, digits = 3, scientific = TRUE),
-                            "\n   b = ", round(F_pred$b,2)), 
-             hjust = -0.5, vjust = 2, size = 4) +
-    annotate("text", x = -Inf, y = Inf, 
-             label = paste0("  Male", 
-                            "\n a = ", format(M_pred$a, digits = 3, scientific = TRUE),
-                            "\n   b = ", round(M_pred$b,2)), 
-             hjust = -0.5, vjust = 3.5, size = 4)+
-  facet_wrap(~ survey)
+    # # ANNOTATIONS
+    # # Add text annotations for slope/intercept
+    # annotate("text", x = -Inf, y = Inf, 
+    #          label = paste0("Female", 
+    #                         "\n a = ", format(F_pred$a, digits = 3, scientific = TRUE),
+    #                         "\n   b = ", round(F_pred$b,2)), 
+    #          hjust = -0.5, vjust = 2, size = 4) +
+    # annotate("text", x = -Inf, y = Inf, 
+    #          label = paste0("  Male", 
+    #                         "\n a = ", format(M_pred$a, digits = 3, scientific = TRUE),
+    #                         "\n   b = ", round(M_pred$b,2)), 
+    #          hjust = -0.5, vjust = 3.5, size = 4)+
   return(plot)
  
   }
