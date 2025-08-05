9a4e98985ba1a20f9e43c3d2b01b15d16fbe10b6
Callie Murakami <muracal2@uw.edu>
2025-08-04 17:24:33 -0700

    changed name of argument "subsurvey"

--- R/plot_dbi.R
+++ R/plot_dbi.R
@@ -1,7 +1,7 @@
 #' Function and formatted data to plot design based index
 #'
 #' @param species common or scientific name of species of interest. See examples for options per dataset.
-#' @param subsurvey subsurvey or survey group, required to specify servey/region to be plotted. See examples for options per dataset.
+#' @param surveys surveys or survey group, required to specify servey/region to be plotted. See examples for options per dataset.
 #' @return a ggplot object, plot of design based indicies
 #' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point theme_bw scale_y_continuous xlab ylab ggtitle
 #' @importFrom dplyr %>% select rename
@@ -30,28 +30,28 @@
 
   
 # DBI plot function
-plot_dbi <- function(species, subsurvey) {
+plot_dbi <- function(species, survey) {
   
   data(all.dbi)
   
   # Assign surveys to larger region center if argument is a single center name
-  if (is.character(subsurvey) && length(subsurvey) == 1) {
-    if (subsurvey == "AK BSAI") {
-      subsurvey <- c("U.S. Aleutian Islands", "U.S. Eastern Bering Sea Slope", 
+  if (is.character(surveys) && length(surveys) == 1) {
+    if (surveys == "AK BSAI") {
+      surveys <- c("U.S. Aleutian Islands", "U.S. Eastern Bering Sea Slope", 
                    "U.S. Eastern Bering Sea Standard Plus NW Region", "U.S. Northern Bering Sea")
-    } else if (subsurvey == "AK GULF") {
-      subsurvey <- "U.S. Gulf of Alaska"
-    }else if (subsurvey == "PBS") {
-      subsurvey <- c("SYN QCS","SYN WCVI", "SYN HS", "SYN WCHG")
-    } else if (subsurvey == "NWFSC") {
-      subsurvey <- c("U.S. West Coast")
+    } else if (surveys == "AK GULF") {
+      surveys <- "U.S. Gulf of Alaska"
+    }else if (surveys == "PBS") {
+      surveys <- c("SYN QCS","SYN WCVI", "SYN HS", "SYN WCHG")
+    } else if (surveys == "NWFSC") {
+      surveys <- c("U.S. West Coast")
     }
   }
   
   
     subset <- all.dbi %>% 
     filter(common_name == species | scientific_name == species) %>%
-    filter(survey %in% subsurvey | region %in% subsurvey) 
+    filter(survey %in% surveys | region %in% surveys) 
   
   
     plot <- ggplot2::ggplot(data = subset) +
@@ -65,7 +65,7 @@ plot_dbi <- function(species, subsurvey) {
       ggplot2::scale_y_continuous(
         labels = function(x) format(x, big.mark = ",", scientific = FALSE))
     
-    if(length(subsurvey) > 1) {
+    if(length(surveys) > 1) {
       plot <- plot + facet_wrap( ~ survey, nrow = 2, scales = "free_y", drop = FALSE) + theme(strip.background = element_blank())
     }
     
