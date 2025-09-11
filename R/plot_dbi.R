3f9f17ed2431acb3a2ef8bde238a7c260a7a7e18
Callie Murakami <muracal2@uw.edu>
2025-09-11 15:08:13 -0700

    add facet arg, empty no data plots

--- R/plot_dbi.R
+++ R/plot_dbi.R
@@ -2,6 +2,7 @@
 #'
 #' @param species common or scientific name of species of interest. See examples for options per dataset.
 #' @param surveys surveys or survey group, required to specify servey/region to be plotted. See examples for options per dataset.
+#' @param facet_all if TRUE this will facet all surveys regardless of missing data, if FALSE then only the region(s) with data will be faceted
 #' @return a ggplot object, plot of design based indicies
 #' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point theme_bw scale_y_continuous xlab ylab ggtitle element_line element_text element_blank theme facet_wrap
 #' @importFrom dplyr %>% select rename
@@ -29,7 +30,7 @@
 #' }
 
 # DBI plot function
-plot_dbi <- function(species, surveys) {
+plot_dbi <- function(species, surveys, facet_all = TRUE) {
 
   
   # Assign surveys to larger region center if argument is a single center name (ex input "PBS" will plot c("SYN QCS","SYN WCVI", "SYN HS", "SYN WCHG"))
@@ -50,7 +51,7 @@ plot_dbi <- function(species, surveys) {
     subset <- fishyplots::all_dbi %>% 
     filter(.data$common_name == species | .data$scientific_name == species) %>%
     filter(.data$survey %in% surveys | .data$region %in% surveys) 
-  
+    
     if (nrow(subset) == 0) {
       return(
         ggplot2::ggplot() + 
@@ -81,10 +82,29 @@ plot_dbi <- function(species, surveys) {
           panel.grid.major.y = element_blank(),
           panel.grid.minor.y = element_blank())
     
-    #facet_wrap if there is more than one survey in the region (BSAI and PBS)
-    if(length(surveys) > 1) {
-      plot <- plot + facet_wrap( ~ survey, nrow = 2, scales = "free_y", drop = FALSE) + theme(strip.background = element_blank())
-    }
+    #facet wrap if plotting all regions
+    if (length(surveys) > 1) {
+      if (facet_all == TRUE) {
+        plot <- plot + facet_wrap( ~ survey, nrow = 2, scales = "free_y", drop = FALSE) + theme(strip.background = element_blank())
+        # find which regions have no data
+        empty_surveys <- setdiff(surveys, unique(subset$survey))
+
+        if (length(empty_surveys) > 0) {
+          # plot "no data" message in empty facets
+          plot <- plot +
+            geom_text(
+              data = data.frame(survey = factor(empty_surveys,
+                                                levels = unique(surveys))),
+              aes(x = mean(range(subset$year)), y = 0, label = "No data"),
+              inherit.aes = FALSE) 
+        }
+      }
+      else if (facet_all == FALSE) {
+        plot <- plot + facet_wrap( ~ survey, nrow = 2, scales = "free_y", drop = FALSE) + theme(strip.background = element_blank())
+      }}
+    
+    
+    
     
     return(plot)
     }
